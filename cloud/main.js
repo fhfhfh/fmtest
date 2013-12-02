6bf5afdfe3a48e1626b1cd9b27afe16de9ed1ea9
fhfhfh <fintan.mahon@feedhenry.com>
2013-12-02 15:12:10 +0000

    mgtest setup

--- cloud/main.js
+++ cloud/main.js
@@ -1,84 +1,23 @@
-//var request = require('request');
-//var jsdom = require('jsdom');
-
-
-exports.getCurrentTime = function(params, callback) {
-
-  return callback(null, { response : 'Sunday, 1 January 1900, 00:00:00' });
-
-/*
-  $fh.cache(
-
-    {
-      act: 'load',
-      key: 'datetime'
-    },
-
-    function(err, res) {
-
-      if (err || !res) {
-        console.log('Returning Time - From WEB');
-        return getCurrentTimeFromWeb(params, callback);
-      }
-
-      else {
-        console.log('Returning Time - From CACHE');
-        return callback(null, { response : res });
-//        return callback(null, { response : res, cached: true });
-      }
-
-    }
-
-  );
+//var util = require('util');
+/* main.js
+ * All calls here are publicly exposed as REST API endpoints.
+ * - all parameters must be passed in a single JSON paramater.
+ * - the return 'callback' method signature is 'callback (error, data)', where 'data' is a JSON object.
 */
 
+/* 'getConfig' server side REST API method.
+ * Trivial example of pulling in a shared config file.
+ */
+exports.getConfig = function(params, callback) {
+  $fh.log({message:'crap1'});
+  console.log("In getConfig() call");
+  var cfg = require("config.js");
+  return callback(null, {config: "hhhhhhhhhh"});
 };
 
-
-function getCurrentTimeFromWeb(params, callback){
-
-/*
-  request(
-
-    {
-      uri: 'http://www.timeanddate.com/worldclock/city.html?n=78'
-    },
-
-    function(err, res, body) {
-
-      console.log(body);
-
-      jsdom.env(
-
-        body,
-
-        ['http://code.jquery.com/jquery.js'],
-
-        function(errors, window) {
-
-          var currentTime = window.$('#ct').text();
-
-          console.log('Current Time: ', currentTime);
-
-          $fh.cache(
-
-            {
-              act: 'save',
-              key: 'datetime',
-              value: currentTime,
-              expire: 10
-            },
-
-            function(err, res) {
-              // Success or failure not so important here - do nothing.
-            }
-
-          );
-
-          return callback(errors, { response : currentTime });
-        }
-      );
-*/
-
+exports.foobar = function(params, callback) {
+  $fh.log({message:'crap2'});
+  return callback(null, {
+    "foo": "bar"
   });
-}
+};
