ace97f3a071eb323dae13ce2eeacf76b1564dd2a
fhfhfh <fintan.mahon@feedhenry.com>
2013-12-02 22:50:25 +0000

    get iscroll working

--- client/default/js/init.js
+++ client/default/js/init.js
@@ -6,15 +6,31 @@ loading the local config and binding a click event to invoke the cloud action
 call which will return the remote config.
 */
 
-$fh.ready(function() {
+$(document).ready(function(){
   // The local config variable from config.js can be accessed directly
   console.log("start js");
+  var myScroll;
   $("#hidden").click(function(){
     $('#main').css('overflow','hidden');
   });
 
   $("#auto").click(function(){
     $('#main').css('overflow','auto');
+  });
+
+  $("#iscroll").click(function(){
+    console.log("start iscroll");
+    myScroll = new iScroll('main',{ bounce: false });
+  });
+
+  $("#refresh").click(function(){
+    console.log("refresh iscroll");
+    myScroll.refresh();
+  });
+
+  $("#destroy").click(function(){
+    console.log("refresh iscroll");
+    myScroll.destroy();
   });
 
 });
