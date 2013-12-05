806e0b8a5a68de84eaa0d76309e1c60d246f993c
fhfhfh <fintan.mahon@feedhenry.com>
2013-12-05 13:54:38 +0000

    sort out bottom scrolling

--- client/default/js/init.js
+++ client/default/js/init.js
@@ -10,22 +10,16 @@ $(document).ready(function(){
   // The local config variable from config.js can be accessed directly
   console.log("start js");
 
-  var myScroll;
-  $("#hidden").click(function(){
-    $('#main').css('overflow','hidden');
-  });
-
-  $("#auto").click(function(){
-    $('#main').css('overflow','auto');
-  });
-
+  var currentElement, parentDiv, posn;
   $("input").click(function(e){
-    console.log("scroll",$('#main').scrollTop());
-    console.log("position",$(e.target).position());
-    var top = e.target.offsetTop-15;
-    // e.target.parentNode.parentNode.scrollTop=e.target.offsetTop-15;
-    $(e.target.parentNode.parentNode).animate({scrollTop: top}, 700);
-
+    currentElement = e.target
+    posn = currentElement.offsetTop-20;
+    parentDiv = $(e.target.parentNode);
+    if ((parentDiv[0].scrollHeight - posn) < parentDiv.height()-20) {
+      //add in spacer
+      $('#scrollSpacer').css({'display':'block', 'height': parentDiv.height()+20+'px'})
+    }
+    $(e.target.parentNode).animate({scrollTop: posn}, 700);
   });
 
 
