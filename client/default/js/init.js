42e917c4fed60dc62497ed2f862f26f48f2a97f8
fhfhfh <fintan.mahon@feedhenry.com>
2013-12-06 09:16:26 +0000

    version which removes spacer

--- client/default/js/init.js
+++ client/default/js/init.js
@@ -1,47 +1,50 @@
-/*
-JSON is automatically included with each app.
-
-Use the $fh.ready() (http://docs.feedhenry.com/wiki/Ready) function to trigger 
-loading the local config and binding a click event to invoke the cloud action 
-call which will return the remote config.
-*/
-
 $(document).ready(function(){
-  // The local config variable from config.js can be accessed directly
   console.log("start js");
 
-  var currentElement, currentElementID="", parentDiv, posn;
+  var currentElement, currentElementID="", parentDiv, posn, spacerHeight=0, distToBottom;
   $("input").click(function(e){
     console.log("handle click" + new Date().getTime() + " " + e.target.id);
     currentElement = e.target;
     currentElementID = e.target.id;
-    posn = currentElement.offsetTop-20;
     parentDiv = $(e.target.parentNode);
-    //check if at bottom of screen and spacer needed
-    console.log("bottom dist "+(parentDiv[0].scrollHeight - posn));
-    console.log("div height  "+parentDiv.height());
     
-    if ((parentDiv[0].scrollHeight - posn) < parentDiv.height()-20) {
+    // get distance to top of scroll of selected element  
+    posn = currentElement.offsetTop - 20 ;
+    // find distance to bottom of scoll of the selected element
+    // check if spacer present and adjust heights if necessary
+    if($('#scrollSpacer').css('display') == "block") {
+      //get spacerheight
+      var height = $('#scrollSpacer').css('height');
+      spacerHeight = height.substring(0,height.length-2);
+    }
+    distToBottom = parentDiv[0].scrollHeight - posn - spacerHeight;
+
+    // check if at bottom of screen and spacer needed
+    if ((distToBottom) < parentDiv.height()-20) {
       //add in spacer
       console.log("add spacer " + (parentDiv.height()-70+'px') )
-      $('#scrollSpacer').css({'display':'block', 'height': parentDiv.height()-70+'px'});
+      $('#scrollSpacer').css({'display':'block', 'height': parentDiv.height()-distToBottom-20+'px'});
+      // if spacer is added set up blur to remove it
+      $(currentElement).on("blur",function(e){
+        console.log("handle blur" + new Date().getTime() + " " + e.target.id);
+        setTimeout(function() {
+          if(currentElementID == e.target.id) {
+            console.log("in blur - remove spacer " + new Date().getTime() + " " + e.target.id);
+            //check if we need to remove spacer
+            if($('#scrollSpacer').css('display') == "block") {
+              // animate page down to allow for removal od spacer
+              var height = $('#scrollSpacer').css('height');
+              posn = $(parentDiv).scrollTop() - height.substring(0,height.length-2);
+              $(e.target.parentNode).animate({scrollTop: posn}, 700, function () {
+                $('#scrollSpacer').css({'display':'none', 'height': ''});
+              });
+            }
+          }
+        },500);
+      });
     }
     //scroll to top
     $(e.target.parentNode).animate({scrollTop: posn}, 700);
-
-    //remove the spacer on blur
-    // $(this).on("blur",function(e){
-    //   console.log("handle blur" + new Date().getTime() + " " + e.target.id);
-    //   console.log("click element "+ currentElementID)
-    //   setTimeout(function() {
-    //     if(currentElementID == e.target.id) {
-    //       console.log("handle blur remove spacer" + new Date().getTime() + " " + e.target.id);
-    //       $('#scrollSpacer').css({'display':'none', 'height': ''});
-    //     }
-    //   },500);
-    // });
-
   });
 
 });
-
